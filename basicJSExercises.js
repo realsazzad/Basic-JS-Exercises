496e9e6388a0eea0056f692756b6a76e5442c4dd
MD Sazzad Hossain <realsazzad@gmail.com>
2018-02-09 00:14:22 +0600

    Complete!

--- basicJSExercises.js
+++ basicJSExercises.js
@@ -63,10 +63,13 @@ HINT: Use substring()
  --------------------------- */
 
 function rotate_string(text) {
-  console.log(text.substr(1));
-  for (var i=text.length;i>0;i--)
+  
+    console.log(text)
+    var x=text;
+  for (var i=0;i<text.length;i++)
   {
-    x=x[x.length-1]+x.substr(1);
+      
+    x=text.substr(text.length-i-1)+text.substr(0,text.length-i-1);
     console.log(x);
 
   }
@@ -75,8 +78,8 @@ function rotate_string(text) {
 
 console.log("Rotate String:");
 /* Uncomment the following to check */
-  rotate_string("cat");
-  //rotate_string("pseudonym")
+  //rotate_string("bird");
+  rotate_string("pseudonym")
 
 
 /* ---------------------------
@@ -94,13 +97,18 @@ HINT: Use split() and substring()
  --------------------------- */
 
 function protect_email(email) {
-  return "protected email";
+    
+    var x=email.split("@");
+    x[0]=x[0].substring(0,3)+"...";
+    x=x[0]+"@"+x[1];
+    
+  return x;
 }
 
 console.log("Protected email:");
 /* Uncomment the following to check */
-  //console.log(protect_email("harry_potter@gmail.com"));
-  //console.log(protect_email("sarah.connor@gmail.com"));
+  console.log(protect_email("harry_potter@gmail.com"));
+  console.log(protect_email("sarah.connor@gmail.com"));
 
 
 /* ---------------------------
@@ -118,13 +126,17 @@ HINT: Use indexOf() and slice()
  --------------------------- */
 
 function remove_first_occurrence(text, searchstring) {
-  return "edited text";
+
+    var i=text.indexOf(searchstring);
+    var x=text.substr(0,i)+text.substr((i+searchstring.length+1));
+    
+  return x;
 }
 
 console.log("Remove First Occurrence:");
 /* Uncomment the following to check */
-  //console.log(remove_first_occurrence("The quick brown fox jumps over the lazy dog", 'the'));
-  //console.log(remove_first_occurrence("Drastic times call for drastic measures", 'drastic'));
+  console.log(remove_first_occurrence("The quick brown fox jumps over the lazy dog", 'the'));
+  console.log(remove_first_occurrence("Drastic times call for drastic measures", 'drastic'));
 
 
 /* ---------------------------
@@ -142,13 +154,17 @@ HINT: Use join(), split() and sort() functions
  --------------------------- */
 
 function alphabetic_order(word) {
-  return "rearranged word";
+  var x=word.split("");
+    x=x.sort();
+    x=x.join("");
+   
+  return x;
 }
 
 console.log("Alphabetic Order:");
 /* Uncomment the following to check */
-  // console.log(alphabetic_order("textbook"));
-  // console.log(alphabetic_order("webmaster"));
+  console.log(alphabetic_order("textbook"));
+  console.log(alphabetic_order("webmaster"));
   // console.log(alphabetic_order("supercalifragilisticexpialidocious"));
 
 
@@ -165,13 +181,31 @@ c occurs 5 times
  --------------------------- */
 
 function most_frequent(arr) {
-  console.log("Most frequently occuring item in arr");
+    var most_f = 1;
+    var c= 0;
+    var item;
+    for (var i=0; i<arr.length; i++)
+    {
+            for (var j=i; j<arr.length; j++)
+            {
+                    if (arr[i] == arr[j])
+                     c++;
+                    if (most_f<c)
+                    {
+                      most_f=c; 
+                      item = arr[i];
+                    }
+            }
+            c=0;
+    }    
+  console.log(item+" occurs "+most_f+" times" );
+    
 }
 
 console.log("Most Frequent Item:");
 /* Uncomment the following to check */
-  // most_frequent([3, 'c', 'c', 'c', 2, 3, 'c', 3, 'c', 2, 4, 9, 3]);
-  // most_frequent([7, 2, 'ax', '9', 9, 'ax', 'ax']);
+ most_frequent([3, 'c', 'c', 'c', 2, 3, 'c', 3, 'c', 2, 4, 9, 3]);
+ most_frequent([7, 2, 'ax', '9', 9, 'ax', 'ax']);
 
 
 /* ---------------------------
@@ -188,12 +222,22 @@ Output:
 
 function remove_duplicates(arr) {
   console.log("Duplicates removed from array");
+    var x="";
+    for(var i=0;i<arr.length;i++)
+        {
+           if(x.indexOf(arr[i])==-1)
+               {
+                   x=x+arr[i];
+               }
+        }
+    arr=x.split("");
+    return arr;
 }
 
-console.log("Remove Duplicate Values:");
+
 /* Uncomment the following to check */
-  // remove_duplicates([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]);
-  // remove_duplicates([4, 4, 4, 5, 's', 8, 's']);
+  console.log(remove_duplicates([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]));
+  console.log(remove_duplicates([4, 4, 4, 5, 's', 8, 's']));
 
 
 /* ---------------------------
@@ -209,15 +253,29 @@ Output:
  --------------------------- */
 
 function dash_in_even(number) {
-  console.log("even numbers separated by dashes");
+  var x=number.toString();
+    var y=x[0];
+    for(var i=1;i<x.length;i++)
+      {
+          if((parseInt(x[i-1])%2==0) && (parseInt(x[i])%2==0) )
+             {
+                 y=y+"-"+x[i];
+                
+             }
+          else
+              {
+                  y+=x[i];
+              }
+      }
+  return y;
 }
 
 console.log("Dash between Even Numbers:");
 /* Uncomment the following to check */
-  // dash_in_even(100);
-  // dash_in_even(1356);
-  // dash_in_even(246824);
-  // dash_in_even(1324567824);
+ //console.log(dash_in_even("01245684"));
+  //console.log(dash_in_even(1356));
+ // console.log(dash_in_even(246824));
+console.log(dash_in_even(1324567824));
 
 
 /* ---------------------------
@@ -232,11 +290,17 @@ HINT: Use Math.ceil() and Math.random()
 
 function guessing_game(guess) {
   // Get a random integer from 1 to 10 inclusive
-  console.log("matched or unmatched?");
+    var x=Math.ceil(10*Math.random());
+    if(x==guess)
+        console.log("Good Work!");
+    else
+        console.log("Not Matched!Value is "+x);
+    return x;
+  
 }
 
 console.log("Guessing Game:");
 /* Uncomment the following to check */
-  // var guess = prompt('Guess the number between 1 and 10 inclusive');
-  // console.log("User guessed: "+ guess);
-  // guessing_game(guess);
+var guess = prompt('Guess the number between 1 and 10 inclusive');
+console.log("User guessed: "+ guess);
+guessing_game(guess);
